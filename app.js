878367cc3c9c8ae18583b87520b042e55a610b54
ayush <ujjwal.msrit@gmail.com>
2019-07-29 18:12:25 +0530

    Made the carousel

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var app=express();
 var bodyParser= require("body-parser");
+
 app.use(bodyParser.urlencoded({extented: true}));
+app.set("view-engine","ejs");
+app.use(express.static("/public"));
 
 app.get("/",function(req,res){
 	res.render("landing.ejs");
 });
 
 
-app.listen(process.env.PORT || 3000 ,process.env.IP,function(){
+
+app.listen(process.env.PORT || 1234 ,process.env.IP,function(){
 	console.log("The EDC server has started");
 });
